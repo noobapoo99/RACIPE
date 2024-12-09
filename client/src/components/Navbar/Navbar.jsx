0b57e3dc19e9fd8072d734e7725a90532bf9469c
Apoorv Nath Tripathi <apoorvtripathi99@gmail.com>
2024-12-09 17:25:29 +0530

    navbar sorted

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,18 @@
 import "./navbar.css";
 function Navbar() {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="left">
+        <a href="/">
+          <span>RACIPE</span>
+        </a>
+        <a href="/">Resources</a>
+        <a href="/">About</a>
+      </div>
+      <div className="right">
+        <a href="/">Right</a>
+      </div>
+    </div>
+  );
 }
 export default Navbar;
